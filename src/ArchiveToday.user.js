8c7d1907ddedf403ac7460c34f4496146d324573
ChrisTorng <ChrisTorng@hotmail.com>
2024-12-19 14:53:38 +0800

    ArchiveToday.user.js adds nautil/washingtonpost. Update README.md.

--- src/ArchiveToday.user.js
+++ src/ArchiveToday.user.js
@@ -10,6 +10,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=archive.is
 // @match        https://archive.is/*
 // @match        https://archive.ph/*
+// @match        https://nautil.us/*
 // @match        https://www.bbc.com/*
 // @match        https://www.bloomberg.com/*
 // @match        https://www.cnbc.com/*
@@ -18,6 +19,7 @@
 // @match        https://www.scientificamerican.com/*
 // @match        https://www.scmp.com/*
 // @match        https://www.thetimes.com/*
+// @match        https://www.washingtonpost.com/*
 // @match        https://www.wsj.com/*
 // @grant        none
 // ==/UserScript==
