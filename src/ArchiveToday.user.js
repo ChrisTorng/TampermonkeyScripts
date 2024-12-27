23f443b161a498b20730ca90cff55bbfd1fc2dfa
ChrisTorng <ChrisTorng@hotmail.com>
2024-12-27 14:20:26 +0800

    Add InternetArchive.user.js for most paywall article redirection. ArchiveToday.user.js keeps only Bloomberg. Update README.md.

--- src/ArchiveToday.user.js
+++ src/ArchiveToday.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ArchiveToday Redirect
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-23_1.1.7
+// @version      2024-12-27_1.2.0
 // @description  Automatically redirect paywall articles to Archive Today
 // @author       ChrisTorng
 // @homepage     https://github.com/ChrisTorng/TampermonkeyScripts/
@@ -10,21 +10,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=archive.is
 // @match        https://archive.is/*
 // @match        https://archive.ph/*
-// @match        https://fortune.com/*
-// @match        https://nautil.us/*
-// @match        https://www.bbc.com/*
 // @match        https://www.bloomberg.com/*
-// @match        https://www.cnbc.com/*
-// @match        https://www.economist.com/*
-// @match        https://www.ft.com/*
-// @match        https://www.nytimes.com/*
-// @match        https://www.scientificamerican.com/*
-// @match        https://www.scmp.com/*
-// @match        https://www.theatlantic.com/*
-// @match        https://www.thetimes.com/*
-// @match        https://www.theverge.com/*
-// @match        https://www.washingtonpost.com/*
-// @match        https://www.wsj.com/*
 // @grant        none
 // ==/UserScript==
 
