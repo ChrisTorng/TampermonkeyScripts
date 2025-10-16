cb09597de1e93f0575ac277942d05e7a8e4decdb
ChrisTorng <ChrisTorng@hotmail.com>
2025-10-17 07:18:42 +0800

    Push history state before redirecting

--- src/Unrollnow.user.js
+++ src/Unrollnow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         X/Twitter to Unrollnow
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-25_1.1.1
+// @version      2025-10-17_1.2.1
 // @description  Redirect X/Twitter links to Unrollnow
 // @author       ChrisTorng
 // @homepage     https://github.com/ChrisTorng/TampermonkeyScripts/
@@ -16,22 +16,36 @@
 (function() {
     'use strict';
     
-    // 檢查是否從 unrollnow.com 來的
-    if (document.referrer.includes('unrollnow.com') ||
-        document.referrer.includes('twitter.com')) {
-        console.log('Already redirected from unrollnow.com, skip redirect');
-        return;
-    }
-
     // 擷取目前網址中的數字 ID
     const urlPattern = /\/status\/(\d+)/;
     const match = window.location.href.match(urlPattern);
 
     if (match && match[1]) {
+        const tweetId = match[1];
+        const storageKey = 'unrollnow-last-redirect';
+
+        const lastRedirectId = sessionStorage.getItem(storageKey);
+
+        if (lastRedirectId === tweetId ||
+            document.referrer.includes('unrollnow.com') ||
+            document.referrer.includes('twitter.com')) {
+            sessionStorage.removeItem(storageKey);
+            console.log('Skip redirect to keep current page in history');
+            return;
+        }
+
         // 組合 Unrollnow 的網址
-        const unrollnowUrl = `https://unrollnow.com/status/${match[1]}`;
-        
-        // 跳轉到新網址
-        window.location.replace(unrollnowUrl);
+        const unrollnowUrl = `https://unrollnow.com/status/${tweetId}`;
+
+        sessionStorage.setItem(storageKey, tweetId);
+
+        try {
+            // 先推入一個 history state，避免即時導向造成無法返回上一頁
+            history.pushState({ redirectedByUnrollnow: true }, '', window.location.href);
+        } catch (error) {
+            console.warn('Unable to push history state before redirect', error);
+        }
+
+        window.location.assign(unrollnowUrl);
     }
 })();
